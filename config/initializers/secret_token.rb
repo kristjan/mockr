aa650137de36992389b3b28d8c2933c6bde8e1f3
Kristján Pétursson <kristjan@gmail.com>
2013-12-15 13:19:18 -0800

    Create Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mockr::Application.config.secret_key_base = '4c4a16a1a9b59f29094ee14cd7b7b9c469cd4595ed41f639e22eb5d585545860fb8942275008f8df64292594e7e38acb2a94139c3744e8fb0dfdfc06f54355bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mockr::Application.config.session_store :cookie_store, key: '_mockr_session'
