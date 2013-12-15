273ec3d6c421d3bfe6c8b39e7d36f10906711d60
Kristján Pétursson <kristjan@gmail.com>
2013-12-15 14:02:30 -0800

    Configure Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,8 @@ Mockr::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = {
+    host: "localhost:#{ENV['PORT'] || 3000}"
+  }
 end
