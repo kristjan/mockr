a303d931de2b8dd78234b1a831e5f15280fe8041
Kristján Pétursson <kristjan@gmail.com>
2013-12-15 22:51:24 -0800

    Validate basic Comment parameters
    Requires an author, mock and body text.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :author, class_name: 'User'
   belongs_to :mock
+
+  validates :author, :mock, :body, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Comment do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with no parameters' do
+    it { should have(1).error_on(:author) }
+    it { should have(1).error_on(:mock) }
+    it { should have(1).error_on(:body) }
+  end
 end
