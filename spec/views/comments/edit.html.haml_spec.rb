ab2af7b7f34cd80cf9fe5ed0916c083da26f4667
Kristján Pétursson <kristjan@gmail.com>
2013-12-15 23:43:34 -0800

    Fix comment form specs
    Author assignment is now handled on the server.

--- spec/views/comments/edit.html.haml_spec.rb
+++ spec/views/comments/edit.html.haml_spec.rb
@@ -8,7 +8,6 @@ describe "comments/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", comment_path(comment), "post" do
-      assert_select "input#comment_author_id[name=?]", "comment[author_id]"
       assert_select "input#comment_mock_id[name=?]", "comment[mock_id]"
       assert_select "textarea#comment_body[name=?]", "comment[body]"
     end
--- spec/views/comments/new.html.haml_spec.rb
+++ spec/views/comments/new.html.haml_spec.rb
@@ -8,7 +8,6 @@ describe "comments/new" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form[action=?][method=?]", comments_path, "post" do
-      assert_select "input#comment_author_id[name=?]", "comment[author_id]"
       assert_select "input#comment_mock_id[name=?]", "comment[mock_id]"
       assert_select "textarea#comment_body[name=?]", "comment[body]"
     end
