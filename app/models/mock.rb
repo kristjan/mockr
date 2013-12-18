e36d2855c15743d3dfd613e7742a21f627b9e62d
Christopher Chan <cchan05@gmail.com>
2013-12-17 18:02:10 -0800

    Clean up rspec test output
    Remove empty test files, disable locales, and use the latest syntax
    for Rails 4 scopes.

--- app/models/mock.rb
+++ app/models/mock.rb
@@ -4,7 +4,7 @@ class Mock < ActiveRecord::Base
   mount_uploader :image, MockUploader
 
   belongs_to :creator, class_name: 'User'
-  has_many :comments, order: 'id DESC'
+  has_many :comments, -> {order('id DESC')}
 
   validates :creator, :image, presence: true
 end
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,8 @@ module Mockr
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.i18n.enforce_available_locales = false
+        
     # Requested by Devise
     config.assets.initialize_on_precompile = false
   end
