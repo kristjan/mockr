2e8ef5e475706b548e92b3fee027b13679d6e790
Christopher Chan <cchan05@gmail.com>
2013-12-22 02:05:07 -0800

    Add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe User do
+  it "has a valid factory" do
+    create(:user).should be_valid
+  end  
+
+  it "is not valid with a blank name" do
+    build(:user, name: "").should_not be_valid
+  end
 end
