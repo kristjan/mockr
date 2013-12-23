45c7b08242b3526b0a2aa7bbaba374c435e8b3b7
Kristján Pétursson <kristjan@gmail.com>
2013-12-23 09:29:20 -0800

    Use Cloudinary for image storage

--- app/uploaders/mock_uploader.rb
+++ app/uploaders/mock_uploader.rb
@@ -2,12 +2,14 @@
 
 class MockUploader < CarrierWave::Uploader::Base
 
+  include Cloudinary::CarrierWave
+
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
