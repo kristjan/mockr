dcc3d32969b61c9fba61876824142aab0d4d0d36
Kristján Pétursson <kristjan@gmail.com>
2014-01-05 13:19:36 -0800

    Add basic user index

--- spec/views/users/index.html.haml_spec.rb
+++ spec/views/users/index.html.haml_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe "users/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe 'users/index.html.haml' do
+  let!(:users) do
+    assign(:users, 2.times.map { create(:user) })
+  end
+
+  it 'renders a list of users' do
+    render
+
+    users.each do |user|
+      assert_select 'tr>td', html: link_to(user.name, user)
+      assert_select 'tr>td', html: mail_to(user.email)
+    end
+  end
 end
